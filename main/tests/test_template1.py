67dc9491f5acf0a6bb9be49655e01e18fecbc4b2
Alex <irememberit@mail.ru>
2025-06-17 16:19:05 +0200

    Made unit tests for template1

---
+++ main/tests/test_template1.py
@@ -0,0 +1,23 @@
+import os
+
+import pytest
+
+
+from main.models.guest import Guest
+from main.models.wedding_info import WeddingInfo
+from main.templates.template1 import Template1
+from main.utils.paths import get_base_dir
+
+wed_info = WeddingInfo(bride_name="Kate Kate", groom_name="Tom Tom", date="23 september", location=" somewhere")
+
+template_obj = Template1(Guest("Daniel Dan"), wed_info)
+
+def test_fill_in():
+    assert type(template_obj.fill_in_wedding_info()) == dict
+    assert len(template_obj.fill_in_wedding_info().items()) == 3
+
+def test_generate():
+    assert template_obj.generate_invitation() == os.path.join(get_base_dir(), "main", "invites", "template1_Daniel_Dan.jpg") # os.path.abspath("main/invites/template1_Daniel_Dan.jpg")
+
+if __name__ == "__main__":
+    pytest.main()
