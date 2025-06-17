941711fc66f8d293c260d8eaaa9b3ba96adaca8c
Alex <irememberit@mail.ru>
2025-06-17 10:57:28 +0200

    Added Guest class

---
+++ main/models/guest.py
@@ -0,0 +1,6 @@
+# Guest class is here, it will contain all the information about a certain guest
+
+class Guest:
+    def __init__(self, first_name: str, last_name: str):
+        self.first_name = first_name
+        self.last_name = last_name
