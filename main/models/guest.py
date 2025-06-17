b1ca3119bafd0415bcfea01d6a3dbe40d30217a3
Alex <irememberit@mail.ru>
2025-06-17 13:42:02 +0200

    Added arial font

--- main/models/guest.py
+++ main/models/guest.py
@@ -3,3 +3,4 @@
 class Guest:
     def __init__(self, full_name: str):
         self.full_name = full_name
+        # in the future it's also possible to add email here to send invitations, that's why I created a different class
