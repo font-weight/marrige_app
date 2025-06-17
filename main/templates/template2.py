f2ac4ea3e0da46fc4a398d33a75eedc4845efc0b
Alex <irememberit@mail.ru>
2025-06-17 13:49:52 +0200

    Added second template without guest name

---
+++ main/templates/template2.py
@@ -0,0 +1,31 @@
+# class and logic for the first template
+from main.models.guest import Guest
+from main.models.template_base import InvitationTemplateBase
+from main.models.wedding_info import WeddingInfo
+
+# here in this template for example I dont want to have guest's name, that's why I used polymorphism
+class Template2(InvitationTemplateBase):
+    def __init__(self,
+                 guest: Guest,
+                 wedding_info: WeddingInfo,
+                 ):
+
+        super().__init__(wedding_info,
+                         "templates/assets/template2.jpg",
+                         "templates/assets/fonts/arial.ttf",
+                         font_size = 30,
+                         groom_coord = (100, 100),
+                         bride_coord = (150, 100),
+                         date_coord = (400, 100),
+                         location_coord = (400, 200)
+                         )
+        self.guest = guest
+
+
+    def generate_invitation(self) -> str:
+        data = self.fill_in_wedding_info()
+        image = data["image"]
+
+        path = f"invites/template1_{self.guest.full_name}.jpg"
+        image.save(path)
+        return path
